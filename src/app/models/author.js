ca839f98ea84ebcc15208236c408fe1d76755967
Le-Quoc-Minh <minhlq@epalsoft.net>
2024-07-27 00:29:21 +0700

    Viết API lấy dữ liệu tài khoản

--- src/app/models/author.js
+++ src/app/models/author.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const Author = new Schema({
     name: { type: String, maxLength: 255, required: true },
-    email: { type: String, maxLength: 255, required: true },
+    email: { type: String, maxLength: 255, required: true, unique: true },
     password: { type: String, maxLength: 255, minLength: 8, required: true },
     role: { type: String, enum: ['admin', 'user'], default: 'user' },
 }, {
--- src/routes/author.js
+++ src/routes/author.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const authorController = require('../app/controllers/AuthorController');
+const { authenticate } = require('../app/middlewares/authorized');
 
+router.get('/fetch-user', authenticate, authorController.fetchUser);
 router.post('/register', authorController.register);
 router.post('/login', authorController.login);
 router.use((req, res, next) => {
