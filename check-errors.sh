12416699c7f5e04058f08028086ed32457ad95ef
Le-Quoc-Minh <minhlq@epalsoft.net>
2024-07-31 01:02:46 +0700

    Bổ sung CI/CD

---
+++ check-errors.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Chạy ứng dụng Node.js
+yarn dev
+
+# Kiểm tra mã thoát của lệnh trước đó
+if [ $? -eq 0 ]; then
+  echo "Ứng dụng đã chạy thành công và không có lỗi."
+else
+  echo "Có lỗi xảy ra khi chạy ứng dụng."
+  exit 1
+fi
