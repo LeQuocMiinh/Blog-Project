69fc0a646ee72d0355cb20d6351fd0ad19a09c39
Le-Quoc-Minh <minhlq@epalsoft.net>
2024-09-17 13:08:18 +0700

    Sửa chức năng lấy bài viết mới nhất

--- src/routes/category.js
+++ src/routes/category.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const { authenticate, authorizeAdmin } = require('../app/middlewares/authorized');
 const categoryController = require('../app/controllers/CategoryController');
 
-router.get('/get-all', authenticate, categoryController.getAllCategories);
 router.get('/get-detail/:id', authenticate, categoryController.getCategoryDetail);
+router.post('/get-all', authenticate, categoryController.getAllCategories);
 router.post('/create', authenticate, authorizeAdmin, categoryController.createCategory);
 router.put('/update/:id', authenticate, authorizeAdmin, categoryController.updateCategory);
 router.put('/trash/:id', authenticate, authorizeAdmin, categoryController.categoryToTrash);
